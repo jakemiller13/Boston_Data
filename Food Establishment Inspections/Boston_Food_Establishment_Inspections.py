c881636c95cbdb059deb892ce915bef0d0a4bf65
Jacob Miller <jmiller@xlrn.com>
2019-05-07 16:30:56 -0400

    Working on cycling through API

--- Food Establishment Inspections/Boston_Food_Establishment_Inspections.py
+++ Food Establishment Inspections/Boston_Food_Establishment_Inspections.py
@@ -18,9 +18,9 @@ from urllib.request import urlopen
 link = 'https://data.boston.gov/datastore/odata3.0/'\
         + '4582bec6-2b4f-4f9e-bc55-cbaa73117f4c'
 
-response = urlopen(link).read().decode('utf-8')
-root = ET.fromstring(response)
+# TODO this API only returns 500 rows at a time, need to loop through it
 
+# Create empty dataframe
 columns = ['id',
            'businessname',
            'dbaname',
@@ -48,27 +48,97 @@ columns = ['id',
            'zip',
            'property_id',
            'location']
+df = pd.DataFrame(columns = columns)
 
-new_list = []
+#####################
+# UTILITY FUNCTIONS #
+#####################
+def inc_address(link, inc):
+    '''
+    Retrieve 1000 rows at a time to avoid timing out
+    '''
+    increment = inc * 1000
+    link = link + '?$skip=' + str(increment) + '&$top=500'
+    return link
+    
+'''
+tree[-1].attrib
+Out[106]: 
+{'rel': 'next',
+ 'href': 'https://data.boston.gov/datastore/odata3.0/4582bec6-2b4f-4f9e-bc55-cbaa73117f4c?$skip=500&$top=500'}
 
-count = 4 # first 4 entries don't carry any info
-while True:
-    new_entry = []
-    try:
-        for child in root[count][-1][-1]:
-            new_entry.append(child.text)
-        new_list.append(new_entry)
-        count += 1
-        print('count = ' + str(count))
-    except IndexError:
-        break
+temp_tree[-1].attrib
+Out[107]: {}
+''' 
+    
+    
+    
 
-df = pd.DataFrame(new_list, columns = columns) # Take care of NaN values
 
-#df = pd.read_csv('tmp_8g7ojgx.csv')
+def get_data(link, df):
+    '''
+    Returns data from link as ElementTree.Element
+    '''
+#    response = urlopen(link).read().decode('utf-8')
+#    tree = ET.fromstring(response)
+#    return tree
+    count = 0
+    while True:
+        link = inc_address(link, count)
+        response = urlopen(link).read().decode('utf-8')
+        tree = ET.fromstring(response)
+        if tree[-1].attrib == {}:
+            temp_df = create_dataframe(tree, start = 4, columns = columns)
+            df.append(temp_df)
+            break
+        else:
+            temp_df = create_dataframe(tree, start = 4, columns = columns)
+            df.append(temp_df)
+            count += 1
+    return df
 
+def create_dataframe(tree, start, columns):
+    '''
+    Returns dataframe created from "tree"
+    Must supply "start" which is root level at which info starts, e.g. "4"
+    "columns" are column headers for dataframe
+    '''
+    info_list = []
+    while True:
+        entry = []
+        try:
+            for child in tree[start][-1][-1]:
+                entry.append(child.text)
+            info_list.append(entry)
+            start += 1
+        except IndexError:
+            break
+    return pd.DataFrame(info_list, columns = columns)
+
+def split_datetime(dttm_col):
+    '''
+    Splits datetime column into 2 separate date & time columns
+    Returns [date, time]
+    '''
+    dttm_split = df[dttm_col].apply(lambda x: x.split())
+
+###############
+# GATHER DATA #
+###############
+tree = get_data(link)
+df = create_dataframe(tree, start = 4, columns = columns)
+
+
+##########################
+# DESIGNATE COLUMN TYPES #
+##########################
 datetime_cols = ['issdttm', 'expdttm', 'resultdttm', 'violdttm']
 str_cols = ['viollevel', 'violstatus', 'violdesc']
+
+df[str_cols] = df[str_cols].astype(str)
+
+######
+
 
 print(df.iloc[0])
 print()
@@ -83,7 +153,7 @@ df.drop(columns = ['dbaname'], inplace = True)
 df.drop(df.loc[df['issdttm'] == ' '].index, inplace = True)
 
 # Force string columns to actually be strings
-df[str_cols] = df[str_cols].astype(str)
+
 
 # Print violation descriptions
 print('\nTypes of violations: \n' + str(np.unique(df['violdesc'])))
